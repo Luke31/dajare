a79adcb90a45edf7740b9c0b38a04b1f9fffda98
Luke31 <lukas.m.schmid@gmail.com>
2020-02-24 18:35:00 +0900

    remove obsolete files

--- emotion/loadmodel.py
+++
@@ -1,8 +0,0 @@
-from metaflow import Flow, get_metadata
-from emotionflow import EmotionPredictor
-import pickle
-
-if __name__ == "__main__":
-    run = Flow("EmotionFlow").latest_successful_run
-    predictor = run['deliver_model'].task.data.predictor
-    pickle.dump(open("emotionpredictor.pkl", "wb"))
